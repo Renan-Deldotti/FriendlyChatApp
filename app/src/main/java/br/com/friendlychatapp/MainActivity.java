384eae202b1f6983ea0d171fa7381d3fe15d5101
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-04-20 15:14:26 -0300

    Initial Files

---
+++ app/src/main/java/br/com/friendlychatapp/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.friendlychatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
