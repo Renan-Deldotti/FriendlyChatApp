c85e3daabb40626ec1d73ee6df6357917e27a806
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-04-26 00:55:18 -0300

    Update MainActivity.java

--- app/src/main/java/br/com/friendlychatapp/MainActivity.java
+++ app/src/main/java/br/com/friendlychatapp/MainActivity.java
@@ -61,6 +61,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
